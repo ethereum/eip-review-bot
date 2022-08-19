125833349d6c95082abe65fa2794819c9a3ef6ad
Pandapip1 <45835846+Pandapip1@users.noreply.github.com>
2022-08-19 15:50:07 -0400

    Add debug information
    Signed-off-by: Pandapip1 <45835846+Pandapip1@users.noreply.github.com>

--- src/rules/authors.ts
+++ src/rules/authors.ts
@@ -4,11 +4,16 @@ import fm from "front-matter";
 export default async function (_octokit: Octokit, _config: Config, files: File[] ) : Promise<Rule[]> {
     // Get results
     let res : Rule[][] = await Promise.all(files.map(async file => {
-        if (!file.filename.endsWith(".md") || !file.filename.startsWith("EIPS")) return [];
+        console.log(`Debug test authors: ${file.filename}`);
+        if (!file.filename.endsWith(".md") || !file.filename.startsWith("EIPS")) {
+            console.log(`Debug: ${file.filename} not EIP`);
+            return [];
+        }
 
         let frontMatter = fm<FrontMatter>((file.previous_contents || file.contents) as string);
 
-        if (file.status.toLowerCase() != 'added' && frontMatter.attributes?.status != "living") { // Living EIPs should only need editor approval
+        if (file.status.toLowerCase() != 'added' && frontMatter.attributes?.status?.toLowerCase() != "living") { // Living EIPs should only need editor approval
+            console.log(`Debug: ${file.filename} author applies ${JSON.stringify(frontMatter.attributes.authors?.split(/\(@(.*?)\)/).filter((_, index) => index % 2 === 1), null, 2)}`);
             return [{
                 name: "authors",
                 reviewers: frontMatter.attributes.authors?.split(/\(@(.*?)\)/).filter((_, index) => index % 2 === 1) || [],
@@ -18,6 +23,7 @@ export default async function (_octokit: Octokit, _config: Config, files: File[]
                 }
             }];
         }
+        console.log(`Debug: ${file.filename} author does not apply; ${file.status.toLowerCase()} ${frontMatter.attributes?.status?.toLowerCase()}`);
 
         return [];
     }));

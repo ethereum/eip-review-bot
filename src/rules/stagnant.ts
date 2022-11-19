98edc993a8f3ae8e4b1e0940f8e03cb0d28591d1
Pandapip1 <45835846+Pandapip1@users.noreply.github.com>
2022-11-19 18:49:48 -0500

    Create stagnant rule
    Signed-off-by: Pandapip1 <45835846+Pandapip1@users.noreply.github.com>

---
+++ src/rules/stagnant.ts
@@ -0,0 +1,30 @@
+import fm from "front-matter";
+import { Octokit, Config, FrontMatter, File, Rule } from "../types.js";
+
+export default async function (_octokit: Octokit, config: Config, files: File[] ) : Promise<Rule[]> {
+    // Get results
+    let res : Rule[][] = await Promise.all(files.map(async file => {
+        if (!file.filename.endsWith(".md")) return [];
+
+        let frontMatter = fm<FrontMatter>(file.previous_contents as string);
+        let status = frontMatter.attributes?.status?.toLowerCase();
+        
+        if (status == "stagnant") {
+            return [{
+                name: "stagnant",
+                reviewers: config[(frontMatterNew.attributes?.category || frontMatterNew.attributes?.type || "all").toLowerCase()],
+                min: 1,
+                annotation: {
+                    file: file.filename
+                }
+            }];
+        }
+
+        return [];
+    }));
+
+    // Merge results
+    let ret: Rule[] = [];
+    res.forEach(val => ret.push(...val));
+    return ret;
+}

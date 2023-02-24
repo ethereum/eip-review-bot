221c6017ed4f9d644bb4fc85b90bf92491f0b1e2
Gavin John <gavinnjohn@gmail.com>
2023-02-24 12:26:41 -0500

    Fix minor issue with test
    Signed-off-by: Gavin John <gavinnjohn@gmail.com>

--- src/rules/__tests__/editorFile.test.ts
+++ src/rules/__tests__/editorFile.test.ts
@@ -1,11 +1,11 @@
 import { Octokit } from "../../types";
-import checkOtherFiles from "../unknown";
+import checkEditorFile from "../editorFile";
 
 let fakeOctokit = null as unknown as Octokit; // Ew, but it works
 
 describe("checkOtherFiles", () => {
     test("Should require governance editors on editor file", () => {
-        expect(checkOtherFiles(fakeOctokit, { governance: ["a", "b", "c"] }, [{ filename: "config/eip-editors.yml", status: "modified" }])).resolves.toMatchObject([{
+        expect(checkEditorFile(fakeOctokit, { governance: ["a", "b", "c"] }, [{ filename: "config/eip-editors.yml", status: "modified" }])).resolves.toMatchObject([{
             name: "unknown",
             reviewers: ["a", "b", "c"],
             min: 2,
@@ -16,6 +16,6 @@ describe("checkOtherFiles", () => {
     });
 
     test("Should not require any reviewers on known file", () => {
-        expect(checkOtherFiles(fakeOctokit, { governance: ["a", "b", "c"] }, [{ filename: "EIPS/foo.txt", status: "modified" }])).resolves.toMatchObject([]);
+        expect(checkEditorFile(fakeOctokit, { governance: ["a", "b", "c"] }, [{ filename: "EIPS/foo.txt", status: "modified" }])).resolves.toMatchObject([]);
     });
 });

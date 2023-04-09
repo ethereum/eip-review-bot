0ab6afd4bc6a1d85ffeda71dafe489b4b8c15cce
Gavin John <gavinnjohn@gmail.com>
2023-04-09 09:49:58 -0400

    Don't change the PR title with insufficient data (#237)
    * Make PR title return undefined if title is not to be changed
    
    Signed-off-by: Gavin John <gavinnjohn@gmail.com>
    
    * Use false to indicate no change
    
    Signed-off-by: Gavin John <gavinnjohn@gmail.com>
    
    ---------
    
    Signed-off-by: Gavin John <gavinnjohn@gmail.com>

--- src/namePr.ts
+++ src/namePr.ts
@@ -56,6 +56,9 @@ export async function generatePRTitle(octokit: Octokit, _: Config, repository: R
     if (files.some(file => file.filename.startsWith("EIPS/eip-") && file.status === "added")) {
         let theFile = files.find(file => file.filename.startsWith("EIPS/eip-") && file.status === "added");
         let frontMatter = fm<FrontMatter>(theFile?.contents as string);
+        if (!frontMatter.attributes?.title) {
+            return false;
+        }
         return localConfig.title.addEipPrefix + frontMatter.attributes?.title;
     }
 
@@ -73,5 +76,5 @@ export async function generatePRTitle(octokit: Octokit, _: Config, repository: R
     }
 
     // Default to the PR title
-    return title;
+    return false;
 }
